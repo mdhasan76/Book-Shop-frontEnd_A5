127de9bef4f2ea5f914d56673b9cc07f3b4dd69e
Md Hasan <mdhasan8064@gmail.com>
2023-07-19 22:05:57 +0600

    install daisy

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      poppins: ["Poppins, sans-serif"],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
